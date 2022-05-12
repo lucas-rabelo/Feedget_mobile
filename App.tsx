9f713d72c033748d4592a7e86fe25bc3dc6fb2ed
ti <lucasrabelodesouza8@gmail.com>
2022-05-12 10:59:09 -0300

    Aula 4

--- App.tsx
+++ App.tsx
@@ -1,20 +1,46 @@
+import 'react-native-gesture-handler';
+
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+
+// components
+import { Widget } from './src/components/Widget';
+
+// styles
+import { theme } from './src/theme';
 
 export default function App() {
+    const [fontsLoaded] = useFonts({
+        Inter_400Regular,
+        Inter_500Medium
+    });
+
+    if (!fontsLoaded) {
+        return <AppLoading />;
+    }
+
     return (
-        <View style={styles.container}>
-            <Text>Open up App.tsx to start working on your app!</Text>
-            <StatusBar style="auto" />
+        <View
+            style={styles.container}
+        >
+            <StatusBar
+                style='light'
+                backgroundColor='transparent'
+                translucent
+            />
+
+            <Widget />
         </View>
     );
 }
 
-const styles = StyleSheet.create({
+
+export const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
+        backgroundColor: theme.colors.background
+    }
+})
